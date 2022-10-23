5195b77c26edf8494f5d9e7def9941b25fdd0031
mmx369 <maksim@klishevich.com>
2022-10-23 21:29:01 +0300

    add current location and pagination

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,40 +1,50 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import BasicAppBar from '../components/BasicAppBar'
-import useFetch from '../hooks/useFetch'
+import { useScroll } from '../hooks/useScroll'
 
 import classes from './NewsPage.module.css'
 
 export function NewsPage() {
-  const url = `http://api.mediastack.com/v1/news?access_key=${process.env.REACT_APP_OPEN_NEWS_FEED_API_KEY}&languages=en&limit=15`
+  const [news, setNews] = useState([])
+  const [offset, setOffset] = useState(0)
+  const LIMIT = '10'
 
-  const { data, loading, error } = useFetch(url)
+  const parentRef = useRef(null)
+  const childRef = useRef(null)
 
-  if (loading)
-    return (
-      <>
-        <BasicAppBar />
-        <div className={classes.header}>Loading...</div>
-      </>
+  const intersected = useScroll(parentRef, childRef, () =>
+    fetchNews(offset, LIMIT)
+  )
+
+  async function fetchNews(page, limit) {
+    const res = await fetch(
+      `http://api.mediastack.com/v1/news?access_key=${process.env.REACT_APP_OPEN_NEWS_FEED_API_KEY}&languages=en&limit=${limit}&offset=${page}`
     )
+    const { data } = await res.json()
+    console.log(1111, res)
+    setNews((prev) => [...prev, ...data])
+    setOffset((prev) => prev + 1)
+  }
 
   return (
     <>
       <BasicAppBar />
-      <div>
+      <div ref={parentRef}>
         <h3 className={classes.header}>News Page</h3>
-        {error && <div>{error}</div>}
-        {data &&
-          data.data.map((el) => (
-            <div className={classes.list} key={el.title}>
+        {news.map((el, i) => (
+          <div className={classes.list} key={i}>
+            <strong>
               {el.author ? `Author: ${el.author}` : `Author: unknown`}
-              <br />
-              <strong>Title: {el.title}</strong>
-              <p>{el.description}</p>
-              {el.image ? (
-                <img src={el.image} alt={el.description} width='300px' />
-              ) : null}
-            </div>
-          ))}
+            </strong>
+            <br />
+            <strong>Title: {el.title}</strong>
+            <p>{el.description}</p>
+            {el.image ? (
+              <img src={el.image} alt={el.description} width='300px' />
+            ) : null}
+          </div>
+        ))}
+        <div ref={childRef} style={{ height: 20, background: 'green' }}></div>
       </div>
     </>
   )
--- src/pages/WeatherPage.jsx
+++ src/pages/WeatherPage.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BasicAppBar from '../components/BasicAppBar'
 import { WeatherCard } from '../components/WeatherCard'
 import useFetch from '../hooks/useFetch'
 import classes from './WeatherPage.module.css'
 
 export function WeatherPage() {
-  const city = 'London'
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`
+  const [myCoords, setMyCoords] = useState({ lon: -0.125, lat: 51.5085 })
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          setMyCoords({
+            lon: position.coords.longitude,
+            lat: position.coords.latitude,
+          })
+        },
+        function (error) {
+          console.error('Error Code = ' + error.code + ' - ' + error.message)
+        }
+      )
+    } else {
+      console.log('Geolocation Is Not Available')
+    }
+  }, [])
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myCoords.lat}&lon=${myCoords.lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`
 
   const { data, loading, error } = useFetch(url)
 
